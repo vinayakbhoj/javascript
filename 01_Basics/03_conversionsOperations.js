ea7ee65de5304066d5fe31674d2516615df59212
@VinayakBhoj <157870477+vinayakbhoj@users.noreply.github.com>
2025-02-27 06:06:42 +0000

    operations

--- 01_Basics/03_conversionsOperations.js
+++ 01_Basics/03_conversionsOperations.js
@@ -1,30 +1,51 @@
-let score = "33jkhjhf"
+// *********** Conversions **************
 
-console.log(typeof score)
+// let score = "33jkhjhf"
 
-let valueInNumber = Number(score)
+// console.log(typeof score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
 
-// "33" => 33
-// "33fd" => NaN
-// true => 1; false => 0
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// // "33" => 33
+// // "33fd" => NaN
+// // true => 1; false => 0
 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanisLoggedIn);
-console.log(booleanisLoggedIn);
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
-// 1 => true; false => 0
-// "" => false; 
-// "luffy" => true
+// // 1 => true; false => 0
+// // "" => false; 
+// // "luffy" => true
 
-let someNumber = ""
+// let someNumber = ""
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+// ************* Operations *************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello "
+let str2 = "luffy"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log();
+
 
