56aa54a3852ac7631186c1965f1b1c86f846b737
@VinayakBhoj <157870477+vinayakbhoj@users.noreply.github.com>
2025-02-27 05:40:43 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionsOperations.js
@@ -0,0 +1,30 @@
+let score = "33jkhjhf"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33fd" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1 => true; false => 0
+// "" => false; 
+// "luffy" => true
+
+let someNumber = ""
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
